57c0370e6b10ff5e75a1766560c88f69c783fa0e
Charalampos Makrylakis <c.makrylakis@gmail.com>
2020-10-13 21:59:35 +0300

    Extract methods

--- twitter-unfollowers.py
+++ twitter-unfollowers.py
@@ -7,6 +7,7 @@ from datetime import datetime
 import tweepy
 
 from constants.logs import FORMAT, DATE_FORMAT
+from constants.twitter import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
 from settings import LOGS_ROOT
 from utils.emails import send_email
 from utils.files import write_json_file, read_json_file
@@ -36,39 +37,56 @@ def handle_rate_limit(cursor):
 
 
 def main():
-    consumer_key = os.environ['TWITTER-API-KEY']
-    consumer_secret = os.environ['TWITTER-API-KEY-SECRET']
-    access_token = os.environ['TWITTER-ACCESS-TOKEN']
-    access_token_secret = os.environ['TWITTER-ACCESS-TOKEN-SECRET']
-
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-
-    api = tweepy.API(auth)
-
     # old followers
     old_data = read_json_file()
     old_followers = old_data.get('followers', {})
 
-    # current followers
-    current_followers = {}
-    for follower in handle_rate_limit(tweepy.Cursor(api.followers).items()):
-        current_followers[str(follower.id)] = follower.screen_name
+    # connect to API
+    api = connect_to_api()
+    api_followers = handle_rate_limit(tweepy.Cursor(api.followers).items())
 
-    data = {
-        'date': datetime.now().strftime('%d/%m/%Y %H:%M:%S'),
-        'count': len(current_followers),
-        'followers': current_followers
-    }
+    # current followers
+    current_followers = mapper(api_followers)
+    data = aggregate(current_followers)
+
     write_json_file(data)
 
     # unfollowers
-    unfollowers = {user_id: old_followers[user_id] for user_id in set(old_followers).difference(set(current_followers))}
+    unfollowers = find_unfollowers(old_followers, current_followers)
 
     print(f'{len(unfollowers)} user(s) unfollowed you {unfollowers if len(unfollowers) > 0 else ""}')
     logger.info(f'{len(unfollowers)} user(s) unfollowed you {unfollowers if len(unfollowers) > 0 else ""}')
 
     # unfollow any unfollowers
+    unfollow(api, unfollowers)
+
+
+def connect_to_api():
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    return tweepy.API(auth)
+
+
+def mapper(followers):
+    current_followers = {}
+    for follower in followers:
+        current_followers[str(follower.id)] = follower.screen_name
+    return current_followers
+
+
+def aggregate(current_followers):
+    return {
+        'date': datetime.now().strftime('%d/%m/%Y %H:%M:%S'),
+        'count': len(current_followers),
+        'followers': current_followers
+    }
+
+
+def find_unfollowers(old_followers, current_followers):
+    return {user_id: old_followers[user_id] for user_id in set(old_followers).difference(set(current_followers))}
+
+
+def unfollow(api, unfollowers):
     for user_id in unfollowers:
         user = api.destroy_friendship(user_id=user_id)
         print(f'Unfollowed {user}')
